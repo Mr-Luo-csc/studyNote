fb4764e5d1f8a25e4e8d41c819312cdf4c4f4ee3
luozhipeng <luozhipeng@nihaosi.com>
2020-05-21 13:53:39 +0800

    动态代理

---
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_JDKProxy/src/main/java/csc/lzp/proxy/ProxyUtil.java
@@ -0,0 +1,14 @@
+package csc.lzp.proxy;
+
+/**
+ * @Discription: 动态代理工具类
+ * 动态代理不是生成一个目标实例对象
+ * 而是将字符串编译一个类实例的过程,通过反射来实现的
+ * @Author: luozhipeng
+ **/
+public class ProxyUtil {
+
+    public static void main(String[] args) {
+
+    }
+}
