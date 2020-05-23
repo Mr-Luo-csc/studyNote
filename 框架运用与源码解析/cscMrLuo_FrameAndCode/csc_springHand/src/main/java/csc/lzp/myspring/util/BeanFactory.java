25bf5d3da02ea24994114bd5d610d056dceab766
luozhipeng <luozhipeng@nihaosi.com>
2020-05-23 19:31:08 +0800

    springIOC

--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springHand/src/main/java/csc/lzp/myspring/util/BeanFactory.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springHand/src/main/java/csc/lzp/myspring/util/BeanFactory.java
@@ -1,14 +1,26 @@
 package csc.lzp.myspring.util;
 
+import org.dom4j.Document;
+import org.dom4j.bean.BeanDocumentFactory;
+
+import java.io.File;
+
 /**
  * @Discription:
  * @Author: luozhipeng
  **/
-public class BeanFactory {
-
-    public static void paresXml(String xml) {
+public class BeanFactory extends BeanDocumentFactory {
 
 
+    public static void parseXml(String xml) {
+        Document doc = null;
+        try {
+            File file = new File("");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static Object getBean() {
