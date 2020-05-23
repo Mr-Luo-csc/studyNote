7fe3072b77d846b29f02b560bd74593fa7bcb826
luozhipeng <luozhipeng@nihaosi.com>
2020-05-23 14:58:46 +0800

    format

---
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springHand/src/main/java/csc/lzp/myspring/util/BeanFactory.java
@@ -0,0 +1,17 @@
+package csc.lzp.myspring.util;
+
+/**
+ * @Discription:
+ * @Author: luozhipeng
+ **/
+public class BeanFactory {
+
+    public static void paresXml(String xml) {
+
+
+    }
+
+    public static Object getBean() {
+        return null;
+    }
+}
