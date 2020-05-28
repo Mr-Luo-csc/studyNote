65b1f9355aab42ea9314afe1d84918a56058b81a
luozhipeng <luozhipeng@nihaosi.com>
2020-05-28 13:24:17 +0800

    format

--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCode/src/main/java/csc/lzp/config/MyConfig.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCode/src/main/java/csc/lzp/config/MyConfig.java
@@ -1,5 +1,6 @@
 package csc.lzp.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * @Author: luozhipeng
  * @Date: 2020/5/28
  **/
+@ComponentScan("csc.lzp")
 @Configuration
 public class MyConfig {
 
--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCode/src/main/java/csc/lzp/test/Test.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCode/src/main/java/csc/lzp/test/Test.java
@@ -12,7 +12,7 @@ public class Test {
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
-        Object csc = context.getBean("csc");
-
+        Object myService = context.getBean("myService");
+        System.out.println("得到容器中的bean" + myService);
     }
 }
