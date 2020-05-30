bb2eab1ac207d68fbfca9f707da2ad4f12e9c3b8
luozhipeng <luozhipeng@nihaosi.com>
2020-05-30 23:22:29 +0800

    factorybean

--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springHand/src/main/java/csc/lzp/factory/test/Test.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springHand/src/main/java/csc/lzp/factory/test/Test.java
@@ -14,6 +14,8 @@ public class Test {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         //context.refresh();
+        //translateBeanName()方法的含义 一个类的复杂关系的时候可以用到factoryBean
+        //mybatis  sqlSessionFactoryBean的使用 @mapperScan是mybatis框架中的注解
         TempFactoryBean tempFactoryBean = (TempFactoryBean) context.getBean("daoFactoryBean");
         tempFactoryBean.test();
     }
