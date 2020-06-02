786048f79aaf29080e5de585dbd703c862b2883c
luozhipeng <luozhipeng@nihaosi.com>
2020-06-02 13:12:04 +0800

    luozhipeng you hear me

---
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCode/src/main/java/csc/lzp/dealtool/MyPostProcessor.java
@@ -0,0 +1,24 @@
+package csc.lzp.dealtool;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Discription: 定义一个自己的后置处理器 这里设置把bean的scope变成圆形的
+ * 这个类是在bean实例化之前起作用的
+ * @Author: luozhipeng
+ * @Date: 2020/6/2
+ **/
+
+@Component
+public class MyPostProcessor implements BeanFactoryPostProcessor {
+
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        BeanDefinition bd = beanFactory.getBeanDefinition("myService");
+        bd.setScope("prototype");
+    }
+
+}
--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCode/src/main/java/csc/lzp/test/Test.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCode/src/main/java/csc/lzp/test/Test.java
@@ -11,7 +11,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test {
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(MyConfig.class);
+        context.refresh();
+
         Object myService = context.getBean("myService");
         System.out.println("得到容器中的bean" + myService);
     }
