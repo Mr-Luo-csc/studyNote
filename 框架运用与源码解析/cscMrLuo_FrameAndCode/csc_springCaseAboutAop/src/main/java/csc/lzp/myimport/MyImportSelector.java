b682b9b42751d4b3e64d14d3efb901b5a79cce55
luozhipeng <luozhipeng@nihaosi.com>
2020-06-24 11:29:30 +0800

    format

--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCaseAboutAop/src/main/java/csc/lzp/myimport/MyImportSelector.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCaseAboutAop/src/main/java/csc/lzp/myimport/MyImportSelector.java
@@ -16,6 +16,11 @@ public class MyImportSelector implements ImportSelector {
         //判断是否加了@enableAop注解
         //annotationMetadata.getMetaAnnotationTypes("");
 
+        //把annotation里面的字段与value，解读出来成map，字段名是key，value为value
+        //AnnotationAttributes attributes = AnnotationConfigUtils.attributesFor(amd, type);
+        //判断annotation是否有效，是否存在作为beanName的字段有value
+        //isStereotypeWithNameValue(v1,v2,v3)方法
+
 //        Set<String> annotationTypes = annotationMetadata.getAnnotationTypes();//得到所有annotation的类名
 //        for (String annotationType : annotationTypes) {
 //            annotationMetadata.getMetaAnnotationTypes(annotationType);
