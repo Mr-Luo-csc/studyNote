23b04242f2bbd98fa3033eeb21db980cb955624f
luozhipeng <luozhipeng@nihaosi.com>
2020-06-24 09:05:44 +0800

    springCase

--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCase/src/main/java/csc/lzp/handler/MyFactoryBean.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCase/src/main/java/csc/lzp/handler/MyFactoryBean.java
@@ -1,8 +1,10 @@
 package csc.lzp.handler;
 
-import csc.lzp.dao.CardDao;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.stereotype.Component;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
@@ -10,16 +12,27 @@ import java.lang.reflect.Proxy;
  * @Author: luozhipeng
  * @Date: 2020/6/16
  **/
+@Component
+public class MyFactoryBean implements FactoryBean, InvocationHandler {
 
-public class MyFactoryBean implements FactoryBean {
+    Class clazz;
+
+    public MyFactoryBean(Class clazz) {
+        this.clazz = clazz;
+    }
 
     public Object getObject() throws Exception {
-        Class[] clazz = {CardDao.class};
-        Object proxy = Proxy.newProxyInstance(this.getClass().getClassLoader(), clazz, new MyInvocationHandler());
+        Class[] clazzs = new Class[]{clazz};
+        Object proxy = Proxy.newProxyInstance(this.getClass().getClassLoader(), clazzs, this);
         return proxy;
     }
 
     public Class<?> getObjectType() {
-        return CardDao.class;
+        return clazz;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("proxy");
+        return null;
     }
 }
