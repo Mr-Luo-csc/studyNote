0e8ce10f533c2967cd9a73b649c0e4ff6f59e84a
luozhipeng <luozhipeng@nihaosi.com>
2020-06-24 10:38:25 +0800

    format

--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCaseAboutAop/src/main/java/csc/lzp/test/Test.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCaseAboutAop/src/main/java/csc/lzp/test/Test.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 /**
  * @Discription:
+ *
+ * ASM 是一个通用的 Java 字节码操作和分析框架。它可以用于修改现有类或直接以二进制形式动态生成类。
+ * ASM 虽然提供与其他 Java 字节码框架如 Javassist，CGLIB类似的功能，但是其设计与实现小而快，且性能足够高。
+ * Spring 直接将 ASM 框架核心源码内嵌于 Spring-core中
+ *
  * @Author: luozhipeng
  * @Date: 2020/6/23
  **/
