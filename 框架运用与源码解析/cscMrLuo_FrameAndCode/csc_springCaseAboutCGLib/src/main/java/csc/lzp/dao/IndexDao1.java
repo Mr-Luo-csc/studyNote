4c2f6e3da4fbec72414f147764242a8637546b16
luozhipeng <luozhipeng@nihaosi.com>
2020-07-01 09:46:15 +0800

    spring中了解bean是如何保证单例的

--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCaseAboutCGLib/src/main/java/csc/lzp/dao/IndexDao1.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCaseAboutCGLib/src/main/java/csc/lzp/dao/IndexDao1.java
@@ -10,6 +10,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class IndexDao1 implements Dao {
 
+    public IndexDao1() {
+        System.out.println("indexDao1的无参构造方法,不用每次使用getBean的时候都去new,Spring中的bean都是单例的");
+    }
+
     public String query() {
         System.out.println("IndexDao1模拟查询数据库");
         return "key_l";
--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCaseAboutCGLib/src/main/java/csc/lzp/dao/IndexDao2.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc_springCaseAboutCGLib/src/main/java/csc/lzp/dao/IndexDao2.java
@@ -7,6 +7,10 @@ package csc.lzp.dao;
  **/
 public class IndexDao2 implements Dao {
 
+    public IndexDao2() {
+        System.out.println("indexDao2的无参构造方法,不用每次使用该bean的时候都去new,保证bean是单例的");
+    }
+
     public String query() {
         System.out.println("IndexDao2模拟查询数据库");
         return "kongFuPang";
