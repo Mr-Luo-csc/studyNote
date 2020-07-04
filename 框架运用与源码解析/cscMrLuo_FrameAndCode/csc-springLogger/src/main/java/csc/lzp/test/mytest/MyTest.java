3a96ec92a1c030a72c001a9c18815253a4264414
luozhipeng <luozhipeng@nihaosi.com>
2020-07-04 17:00:11 +0800

    拷贝,beanUtils拷贝失败,待优化

---
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc-springLogger/src/main/java/csc/lzp/test/mytest/MyTest.java
@@ -0,0 +1,55 @@
+package csc.lzp.test.mytest;
+
+import org.springframework.beans.BeanUtils;
+
+/**
+ * @Discription:
+ * @Author: luozhipeng
+ * @Date: 2020/7/4
+ **/
+public class MyTest {
+
+    private String setter;
+    private String msg;
+
+    //-------------------克隆方法(浅拷贝)--------------------//
+    public static MyTest copyBean(MyTest sourceObj) {
+        MyTest target = new MyTest();
+        BeanUtils.copyProperties(sourceObj, target);
+        return target;
+    }
+
+    //-------------------判空方法--------------------//
+    public static void isEmpty() {
+
+    }
+
+    //-------------------Main测试--------------------//
+    public static void main(String[] args) {
+        MyTest myTest = new MyTest();
+        myTest.setSetter("罗志祥");
+        myTest.setMsg("Cookie");
+        MyTest takeTarget = MyTest.copyBean(myTest);
+        System.out.println("得到克隆之后的对象: " + takeTarget.toString());
+        System.out.println("sourceObj == targetObj: " + (myTest == takeTarget));
+    }
+
+    //-------------------Setter方法--------------------//
+    public void setSetter(String setter) {
+        this.setter = setter;
+    }
+
+    //-------------------Setter方法--------------------//
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    //-------------------重写toString方法--------------------//
+    @Override
+    public String toString() {
+        return "MyTest{" +
+                "setter='" + setter + '\'' +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
