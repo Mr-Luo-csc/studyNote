801b011959fdfa96b6bb4808802f6d33b0c80f6c
luozhipeng <luozhipeng@nihaosi.com>
2020-07-04 17:06:13 +0800

    fix 浅拷贝 还是失败

--- 框架运用与源码解析/cscMrLuo_FrameAndCode/csc-springLogger/src/main/java/csc/lzp/test/mytest/MyTest.java
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc-springLogger/src/main/java/csc/lzp/test/mytest/MyTest.java
@@ -25,13 +25,19 @@ public class MyTest {
     }
 
     //-------------------Main测试--------------------//
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         MyTest myTest = new MyTest();
         myTest.setSetter("罗志祥");
         myTest.setMsg("Cookie");
         MyTest takeTarget = MyTest.copyBean(myTest);
         System.out.println("得到克隆之后的对象: " + takeTarget.toString());
         System.out.println("sourceObj == targetObj: " + (myTest == takeTarget));
+    }*/
+    public static void main(String[] args) {
+        MyTest myTest = new MyTest("csc","222");
+        MyTest myTest1 = new MyTest();
+        BeanUtils.copyProperties(myTest, myTest1);
+        System.out.println(myTest1.toString());
     }
 
     //-------------------Setter方法--------------------//
@@ -51,5 +57,15 @@ public class MyTest {
                 "setter='" + setter + '\'' +
                 ", msg='" + msg + '\'' +
                 '}';
+    }
+
+    //-------------------无参构造方法--------------------//
+    public MyTest() {
+    }
+
+    //-------------------带参构造方法--------------------//
+    public MyTest(String setter, String msg) {
+        this.setter = setter;
+        this.msg = msg;
     }
 }
