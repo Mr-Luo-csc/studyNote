7c40be8f71e7178b1b8fa7e31fa52ff79896e36a
luozhipeng <luozhipeng@nihaosi.com>
2020-07-20 09:44:49 +0800

    特殊的bean后置处理器

---
+++ 框架运用与源码解析/cscMrLuo_FrameAndCode/csc-springAopCase/src/main/java/csc/lzp/handler/MyInstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,15 @@
+package csc.lzp.handler;
+
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Discription: 这个bean的后置处理器的作用是:创建一个bean,bean中的属性没有被填充
+ * @Author: luozhipeng
+ * @Date: 2020/7/20
+ **/
+//@Component
+public class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+
+
+}
