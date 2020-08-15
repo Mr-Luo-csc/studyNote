1ba275f49399d782401f54a6e4fbdcb50a41a3c2
luozhipeng <luozhipeng@nihaosi.com>
2020-08-15 12:02:38 +0800

    JDk observer添加与执行的顺序

--- SpringBoot应用与解析/SpringBoot-Case/my-Observer/src/main/java/csc/lzp/jdk/test/JDKTest.java
+++ SpringBoot应用与解析/SpringBoot-Case/my-Observer/src/main/java/csc/lzp/jdk/test/JDKTest.java
@@ -13,6 +13,7 @@ public class JDKTest {
 
     public static void main(String[] args) {
         MovieJDK movieJDK = new MovieJDK();
+        //todo 看这个Observer的执行顺序,先添加的观察者,后执行
         movieJDK.addObserver(new ViewerAJDK());
         movieJDK.addObserver(new ViewerBJDK());
         movieJDK.move();
