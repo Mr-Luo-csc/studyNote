047c8fc5749931fd50af667ffc098921b448f591
luozhipeng <luozhipeng@nihaosi.com>
2020-08-15 15:50:55 +0800

    notifyObservers()方法中可以传入事件对象

--- SpringBoot应用与解析/SpringBoot-Case/my-Observer/src/main/java/csc/lzp/jdk/observed/MovieJDK.java
+++ SpringBoot应用与解析/SpringBoot-Case/my-Observer/src/main/java/csc/lzp/jdk/observed/MovieJDK.java
@@ -12,7 +12,7 @@ public class MovieJDK extends Observable {
     //电影出现打斗动作时触发的方法
     public void move() {
         //todo 调用以下两个方法触发观察者
-        //todo JDK好像封装了事件?
+        //todo JDK好像封装了事件? answer: notifyObservers()方法中可以传入一个参数,我们用来传入事件对象
         setChanged();
         notifyObservers();
     }
