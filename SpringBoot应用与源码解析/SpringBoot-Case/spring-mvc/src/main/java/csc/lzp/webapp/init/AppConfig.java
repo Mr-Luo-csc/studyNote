b8b2c3ecf171c24786e1713273abb29104420adb
luozhipeng <luozhipeng@nihaosi.com>
2020-08-18 18:26:52 +0800

    add spring-mvc case

---
+++ SpringBoot应用与源码解析/SpringBoot-Case/spring-mvc/src/main/java/csc/lzp/webapp/init/AppConfig.java
@@ -0,0 +1,16 @@
+package csc.lzp.webapp.init;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @Author: luozhipeng
+ * @Date: 2020/8/18
+ **/
+@Configuration
+@ComponentScan("csc.lzp.webapp")
+//@EnableWebMvc todo 这里了解一下 以前的web项目需要添加这个注解
+public class AppConfig {
+
+}
