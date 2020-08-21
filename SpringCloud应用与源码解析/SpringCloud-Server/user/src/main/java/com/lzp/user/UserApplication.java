a0e26f76222fe12f2c68778e0f0b5d31945c5ae9
luozhipeng <luozhipeng@nihaosi.com>
2020-08-21 17:47:57 +0800

    add runClass

---
+++ SpringCloud应用与源码解析/SpringCloud-Server/user/src/main/java/com/lzp/user/UserApplication.java
@@ -0,0 +1,18 @@
+package com.lzp.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: luozhipeng
+ * @Date: 2020/8/21
+ **/
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
--- SpringCloud应用与源码解析/SpringCloud-Server/user/src/main/java/com/lzp/user/config/AppConfig.java
+++ SpringCloud应用与源码解析/SpringCloud-Server/user/src/main/java/com/lzp/user/config/AppConfig.java
@@ -1,9 +1,31 @@
 package com.lzp.user.config;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * @Description:
  * @Author: luozhipeng
  * @Date: 2020/8/21
  **/
+@Configuration
+@ComponentScan("com.lzp.user")
 public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    //======设置tomcat的端口号
+    @Bean
+    public TomcatServletWebServerFactory tomcat() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.setPort(8081);
+        return tomcat;
+    }
+
 }
