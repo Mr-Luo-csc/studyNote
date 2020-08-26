c778f738920c70679add46df0c6b6089bf5516d0
luozhipeng <luozhipeng@nihaosi.com>
2020-08-26 13:49:41 +0800

    增加power服务,模拟power服务集群

---
+++ SpringCloud应用/SpringCloud-Case/power1/src/main/java/com/lzp/power1/Power1Application.java
@@ -0,0 +1,17 @@
+package com.lzp.power1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: luozhipeng
+ * @Date: 2020/8/25
+ **/
+@SpringBootApplication
+public class Power1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Power1Application.class, args);
+    }
+}
---
+++ SpringCloud应用/SpringCloud-Case/power1/src/main/java/com/lzp/power1/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.lzp.power1.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @Author: luozhipeng
+ * @Date: 2020/8/25
+ **/
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public TomcatServletWebServerFactory tomcat() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.setPort(6000);
+        return tomcat;
+    }
+}
