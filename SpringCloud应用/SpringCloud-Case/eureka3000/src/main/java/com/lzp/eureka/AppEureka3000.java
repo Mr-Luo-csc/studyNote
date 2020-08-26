9b22bae007d1b31e0ffa2b98ac7e1d8a8c4e8df6
luozhipeng <luozhipeng@nihaosi.com>
2020-08-26 14:01:13 +0800

    rename eureka模块

--- SpringCloud应用/SpringCloud-Case/eureka3000/src/main/java/com/lzp/eureka/AppEureka3000.java
+++ SpringCloud应用/SpringCloud-Case/eureka3000/src/main/java/com/lzp/eureka/AppEureka3000.java
@@ -11,10 +11,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  **/
 @SpringBootApplication
 @EnableEurekaServer
-public class AppEureka {
+public class AppEureka3000 {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppEureka.class, args);
+        SpringApplication.run(AppEureka3000.class, args);
     }
 
 }
