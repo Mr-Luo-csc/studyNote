54f5a764c32ba5f89d790058060f1161ca35fa35
luozhipeng <luozhipeng@nihaosi.com>
2020-08-28 10:36:47 +0800

    添加随机load-balance

---
+++ SpringCloud应用/z-load-balance/src/main/java/com/lzp/balance/constant/ServerIps.java
@@ -0,0 +1,9 @@
+package com.lzp.balance.constant;
+
+/**
+ * @Description:
+ * @Author: luozhipeng
+ * @Date: 2020/8/28
+ **/
+public class ServerIps {
+}
---
+++ SpringCloud应用/z-load-balance/src/main/java/com/lzp/balance/随机/Random.java
@@ -0,0 +1,9 @@
+package com.lzp.balance.随机;
+
+/**
+ * @Description:
+ * @Author: luozhipeng
+ * @Date: 2020/8/28
+ **/
+public class Random {
+}
