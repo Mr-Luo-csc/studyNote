5a7806229a638893568a13fe46afaf0484330277
luozhipeng <luozhipeng@nihaosi.com>
2020-08-28 10:37:23 +0800

    add 随机load-balance

--- SpringCloud应用/z-load-balance/src/main/java/com/lzp/balance/constant/ServerIps.java
+++ SpringCloud应用/z-load-balance/src/main/java/com/lzp/balance/constant/ServerIps.java
@@ -1,9 +1,26 @@
 package com.lzp.balance.constant;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * @Description:
+ * @Description: 模拟服务器
  * @Author: luozhipeng
  * @Date: 2020/8/28
  **/
 public class ServerIps {
+
+    public static final List<String> LIST = Arrays.asList(
+            "192.168.0.1",
+            "192.168.0.2",
+            "192.168.0.3",
+            "192.168.0.4",
+            "192.168.0.5",
+            "192.168.0.6",
+            "192.168.0.7",
+            "192.168.0.8",
+            "192.168.0.9",
+            "192.168.0.10"
+    );
+
 }
--- SpringCloud应用/z-load-balance/src/main/java/com/lzp/balance/随机/Random.java
+++ SpringCloud应用/z-load-balance/src/main/java/com/lzp/balance/随机/Random.java
@@ -1,9 +1,26 @@
 package com.lzp.balance.随机;
 
+import com.lzp.balance.constant.ServerIps;
+
 /**
  * @Description:
  * @Author: luozhipeng
  * @Date: 2020/8/28
  **/
 public class Random {
+
+    //======负载均衡算法实现的方法
+    public static String getServer() {
+        java.util.Random random = new java.util.Random();
+        int randomPos = random.nextInt(ServerIps.LIST.size());
+        return ServerIps.LIST.get(randomPos);
+    }
+
+    //======测试连续调用十次
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(getServer());
+        }
+    }
+
 }
