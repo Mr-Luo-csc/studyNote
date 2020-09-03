49a554dce1fd7f733b9b0ee1a5663facfd33d3f5
juhuizhong <juhuizhong@nihaosi.com>
2020-09-03 13:26:14 +0800

    zuul回退

--- SpringCloud应用/SpringCloud-Case/zuul/src/main/java/com/lzp/zuul/provider/PowerFallbackProvider.java
+++ SpringCloud应用/SpringCloud-Case/zuul/src/main/java/com/lzp/zuul/provider/PowerFallbackProvider.java
@@ -1,8 +1,16 @@
 package com.lzp.zuul.provider;
 
+import com.netflix.hystrix.exception.HystrixTimeoutException;
 import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @Description: 容错回退
@@ -15,14 +23,57 @@ public class PowerFallbackProvider implements FallbackProvider {
 
     @Override
     public String getRoute() {
-        return "power";
+        return "server-power";
     }
 
     //======回退的逻辑回调的方法
     @Override
     public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
 
+        if (cause instanceof HystrixTimeoutException) {
+            //网关超时
+            return response(HttpStatus.GATEWAY_TIMEOUT);
+        } else {
+            //内部服务器错误
+            return response(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
-        return null;
+    private ClientHttpResponse response(final HttpStatus status) {
+
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return status;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return status.value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return status.getReasonPhrase();
+            }
+
+            //============================todo 以下三个方法比较重要===============================//
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("系统繁忙,请稍后再试...".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                return httpHeaders;
+            }
+        };
     }
 }
