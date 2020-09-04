3957d3fa1242a76e182e54eab954f2d3bbb9a463
juhuizhong <juhuizhong@nihaosi.com>
2020-09-04 14:11:16 +0800

    add hystrix dashbord

---
+++ SpringCloud应用/SpringCloud-Case/hystrix-dashbord/src/main/java/com/lzp/dashbord/AppHystrixDashbord.java
@@ -0,0 +1,19 @@
+package com.lzp.dashbord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Description:
+ * @Author: luozhipeng
+ * @Date: 2020/9/4
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class AppHystrixDashbord {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppHystrixDashbord.class, args);
+    }
+}
