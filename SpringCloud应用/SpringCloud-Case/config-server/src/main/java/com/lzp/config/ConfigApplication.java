3c1e6c822e6efc84c29df69d136bdb794f57411f
juhuizhong <juhuizhong@nihaosi.com>
2020-09-07 16:42:27 +0800

    config-server 启动类加注解

--- SpringCloud应用/SpringCloud-Case/config-server/src/main/java/com/lzp/config/ConfigApplication.java
+++ SpringCloud应用/SpringCloud-Case/config-server/src/main/java/com/lzp/config/ConfigApplication.java
@@ -1,9 +1,19 @@
 package com.lzp.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * @Description:
  * @Author: luozhipeng
  * @Date: 2020/9/7
  **/
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
 }
--- SpringCloud应用/SpringCloud-Case/config-server1/src/main/java/com/lzp/config1/Config1Application.java
+++ SpringCloud应用/SpringCloud-Case/config-server1/src/main/java/com/lzp/config1/Config1Application.java
@@ -1,9 +1,19 @@
 package com.lzp.config1;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * @Description:
  * @Author: luozhipeng
  * @Date: 2020/9/7
  **/
+@SpringBootApplication
+@EnableConfigServer
 public class Config1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config1Application.class, args);
+    }
 }
