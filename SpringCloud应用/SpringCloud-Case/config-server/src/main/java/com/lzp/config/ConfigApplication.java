278f18708c2073237905af9d9a676be51b3bea4f
juhuizhong <juhuizhong@nihaosi.com>
2020-09-07 15:52:13 +0800

    add config-server集群

---
+++ SpringCloud应用/SpringCloud-Case/config-server/src/main/java/com/lzp/config/ConfigApplication.java
@@ -0,0 +1,9 @@
+package com.lzp.config;
+
+/**
+ * @Description:
+ * @Author: luozhipeng
+ * @Date: 2020/9/7
+ **/
+public class ConfigApplication {
+}
---
+++ SpringCloud应用/SpringCloud-Case/config-server1/src/main/java/com/lzp/config1/Config1Application.java
@@ -0,0 +1,9 @@
+package com.lzp.config1;
+
+/**
+ * @Description:
+ * @Author: luozhipeng
+ * @Date: 2020/9/7
+ **/
+public class Config1Application {
+}
